3c5aed8de8f68bafa00b58c3ab7398afd5904b67
Carlos Gutiérrez <carlosgutimo.3d@gmail.com>
2022-07-08 17:29:38 +0200

    updating tests

--- test/usefull_extensions_test.dart
+++ test/usefull_extensions_test.dart
@@ -1,15 +1,174 @@
-// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:usefull_extensions/usefull_extensions.dart';
+
+import 'helpers/pump_app.dart';
 
 void main() {
-  group('UsefullExtensions', () {
-    test('can be instantiated', () {});
+  group('Strings extensions', () {
+    const text = 'exception test';
+    test('Capitalized text', () {
+      final result = text.toCapitalized();
+
+      expect(
+        result,
+        equals('Exception test'),
+      );
+    });
+
+    test('Capitalized without text', () {
+      const String? test = null;
+      final result = test?.toCapitalized();
+
+      expect(
+        result,
+        equals(null),
+      );
+    });
+
+    test('TitleCase with text', () {
+      final result = text.toTitleCase();
+
+      expect(
+        result,
+        equals('Exception Test'),
+      );
+    });
+
+    test('TitleCase without text', () {
+      const String? test = null;
+      final result = test?.toTitleCase();
+
+      expect(
+        result,
+        equals(null),
+      );
+    });
   });
 
-  group('UsefullWidgets', () {
-    test('can be instantiated', () {});
+  group('Widgets extensions', () {
+    const items = <Widget>[
+      Text('a'),
+      Text('b'),
+      Text('c'),
+      Text('d'),
+    ];
+    test('Adding separator', () {
+      final result = items.joinWithSeparator(HorizontalSpacer.semi());
+
+      expect(
+        result.length,
+        equals(7),
+      );
+    });
+
+    test('Adding separator on a single widget', () {
+      final result = [const Text('a')].joinWithSeparator(
+        HorizontalSpacer.semi(),
+      );
+
+      expect(
+        result.length,
+        equals(1),
+      );
+    });
+
+    test('Adding space should equal same ammount of Widgets, but with padding',
+        () {
+      final result = items.spaced();
+
+      expect(
+        result.length,
+        equals(items.length),
+      );
+    });
+
+    test('Adding a Spacer widget', () {
+      final result = [...items, const Spacer()].spaced();
+
+      expect(
+        result.length,
+        equals(items.length + 1),
+      );
+    });
+
+    testWidgets(
+      'Finds padding when spacing the widgets',
+      (tester) async {
+        await tester.pumpApp(
+          Column(children: items.spaced()),
+        );
+        expect(find.byType(Padding), findsNWidgets(items.length));
+      },
+    );
+    testWidgets(
+      '''Finds paddings when spacing the widgets, doesnt add padding to this to Spacer''',
+      (tester) async {
+        await tester.pumpApp(
+          Column(children: [...items, const Spacer()].spaced()),
+        );
+        expect(find.byType(Padding), findsNWidgets(items.length));
+      },
+    );
   });
-  group('UsefullDimens', () {
-    test('can be instantiated', () {});
+
+  group('Object extensions', () {
+    test('Log on object', () {
+      final strLen = 'MyOutput'.log();
+
+      expect(strLen, 8);
+    });
+    test('Unwrapped string on object?', () {
+      dynamic model;
+
+      model = null.unwrappedString();
+      expect(model, '-');
+
+      model = null.unwrappedString('?');
+      expect(model, '?');
+
+      model = 'test'.unwrappedString();
+      expect(model, 'test');
+
+      model = true;
+      expect(model, true);
+      // TODO(carlos): !(String?)
+    });
+  });
+
+  group('Object Map<K,V>', () {
+    test('where extension', () {
+      final people = <String, int>{
+        'John': 20,
+        'Mary': 21,
+        'Peter': 22,
+      };
+
+      final subMap = people.where((key, value) => key.length > 4 && value > 20);
+
+      expect(subMap, {'Peter': 22});
+    });
+    test('whereKey extension', () {
+      final people = <String, int>{
+        'John': 20,
+        'Mary': 21,
+        'Peter': 22,
+      };
+
+      final subMap = people.whereKey((key) => key.length < 5);
+
+      expect(subMap, {'John': 20, 'Mary': 21});
+    });
+    test('whereValue extension', () {
+      final people = <String, int>{
+        'John': 20,
+        'Mary': 21,
+        'Peter': 22,
+      };
+
+      final subMap = people.whereValue((value) => value.isEven);
+
+      expect(subMap, {'John': 20, 'Peter': 22});
+    });
   });
 }
